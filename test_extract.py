ba8800482eb08fd2ee79049ecc0e07046e4737e3
lixuze <intoblack86@gmail.com>
2014-04-23 23:37:13 +0800

    添加提取title

--- test_extract.py
+++ test_extract.py
@@ -11,36 +11,59 @@ from vampire.htmlextract import HtmlExtract
 class Mosquito(object):
 
     __news__ = get_news_json()
-    __extract = HtmlExtract()
+    __html_extract = HtmlExtract()
 
     def __getattr__(self, key):
-    	urls = []
-    	__url_set = set()
+        urls = []
+        __url_set = set()
         if self.__news__.has_key(key):
             if self.__news__[key].has_key('base_url'):
+                print 'download......'
                 html = network.get_html_string(self.__news__[key]['base_url'])
+                print 'download finish .....'
                 soup = BeautifulSOAP(html)
+                # print html
                 if soup:
-                	__filter = self.__news__[key].has_key('filter')
-                	for __rule in self.__news__[key]['url_rule']:
-                		for __child in soup.findAll(__rule ['tag'] , attrs = __rule['attrs']):
-                			__url = __child.get('href')
-                			if __url in __url_set:
-                				continue
-                			__url_set.add(__url)
-                			if __filter:
-                				if self.__news__[key]['filter'].match(__url):
-
-                				    urls.append(__url)
-                			else:
-                				urls.append(__url)
+                    print 'x'
+                    __filter = self.__news__[key].has_key('filter')
+                    for __rule in self.__news__[key]['url_rule']:
+                        for __child in soup.findAll(__rule['tag'], attrs=__rule['attrs']):
+                            __url = __child.get('href')
+                            if __url in __url_set:
+                                continue
+                            __url_set.add(__url)
+                            if __filter:
+                                if self.__news__[key]['filter'].match(__url):
+                                    urls.append(self.__extract(__url))
+                            else:
+                                urls.append(self.__extract(__url))
         return urls
 
-    def extract(self , url ):
-    	if url and len(url) > 0 and url[:4] == 'http':
-    		html_string = network.get_html_string(url)
-    		return {'content' :self.__extract.get_text(html_string)}
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+
+
+    def __get_title(self , soup):
+        if soup :
+            return soup.head.title.string
+        return None
+    def __extract(self, url):
+        if url and len(url) > 0 and url[:4] == 'http':
+            try:
+                news = {}
+                html_string = network.get_html_string(url)
+                soup = BeautifulSOAP(html_string)
+                news['title'] = self.__get_title(soup)
+                content = self.__html_extract.get_text(html_string)
+                news['content'] = content 
+                return 
+            except Exception, e:
+                print e
+        return None
+
+
 if __name__ == '__main__':
 
     m = Mosquito()
-    print m.extract(m.baidu[1])['content']
+    print m['baidu']
+    print 'get....'
