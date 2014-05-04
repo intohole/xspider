ac353cf0b94a97660b1ea0e45e4c9d900403563f
lixuze <intoblack86@gmail.com>
2014-05-04 22:14:08 +0800

    修正title 无法提取的问题

--- test_extract.py
+++ test_extract.py
@@ -2,26 +2,28 @@
 #!/usr/bin/env python
 
 
-from BeautifulSoup import BeautifulSOAP
+from BeautifulSoup import BeautifulSoup
 from vampire.utils import network
 from webnews import get_news_json
 from vampire.htmlextract import HtmlExtract
+import re
 
 
 class Mosquito(object):
 
+    __title = re.compile(u'<[ ]{0,}title[ ]{0,}>(.*?)<[ ]{0,}/tilte[ ]{0 ,}>' , re.I)
     __news__ = get_news_json()
     __html_extract = HtmlExtract()
 
     def __getattr__(self, key):
-        urls = []
+        content = []
         __url_set = set()
         if self.__news__.has_key(key):
             if self.__news__[key].has_key('base_url'):
                 print 'download......'
                 html = network.get_html_string(self.__news__[key]['base_url'])
                 print 'download finish .....'
-                soup = BeautifulSOAP(html)
+                soup = BeautifulSoup(html)
                 # print html
                 if soup:
                     print 'x'
@@ -34,33 +36,46 @@ class Mosquito(object):
                             __url_set.add(__url)
                             if __filter:
                                 if self.__news__[key]['filter'].match(__url):
-                                    urls.append(self.__extract(__url))
+                                    content.append(self.__extract(__url))
                             else:
-                                urls.append(self.__extract(__url))
-        return urls
+                                content.append(self.__extract(__url))
+        return content
 
     def __getitem__(self, key):
         return self.__getattr__(key)
 
-
-    def __get_title(self , soup):
-        if soup :
-            return soup.head.title.string
+    def __get_title(self, soup):
+        if soup:
+            return soup.title.string
         return None
+
     def __extract(self, url):
         if url and len(url) > 0 and url[:4] == 'http':
             try:
                 news = {}
                 html_string = network.get_html_string(url)
-                soup = BeautifulSOAP(html_string)
+                soup = BeautifulSoup(html_string)
                 news['title'] = self.__get_title(soup)
                 content = self.__html_extract.get_text(html_string)
-                news['content'] = content 
-                return 
+                news['content'] = content
+                # news['title'] = self.__extract_title(html_string)
+                print news
+                return news
             except Exception, e:
                 print e
         return None
 
+    def __extract_title(self, html):
+        if html and html != '':
+            if isinstance(html, (str, unicode)):
+                 print html
+                 m = self.__title.search(html)
+                 print 'm'
+                 print m
+                 if m:
+                    print 'title'
+                    return m.group(0)
+        return None
 
 if __name__ == '__main__':
 
