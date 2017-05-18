2859deecd289d6992133a0f216b75c77f4de8417
ianhhhhhhhhe <517382432@qq.com>
2017-05-18 15:32:41 +0800

    Add some more exceptions

--- xspider/libs/exception.py
+++ xspider/libs/exception.py
@@ -2,7 +2,7 @@
 
 
 
-class SpiderException(Exception):
+class BaseException(Exception):
     """xspider异常定义类
         try:
             int("1212s")
@@ -11,8 +11,24 @@ class SpiderException(Exception):
     """
 
     def __init__(self ,msg):
-        super(SpiderException , self).__init__()
+        super(BasException , self).__init__()
         self.msg = msg
 
     def __str__(self):
         return "error :[{error}]".format(error = self.msg)
+
+
+class URLException(BaseException):
+    """Raise this when a URL is illegal"""
+    pass
+
+class SpiderException(BaseException):
+    """Old exception"""
+    pass
+
+class CloseSpider(BaseException):
+    """Raise this from callbacks to request the spider to be closed"""
+
+    def __init__(self, reason='cancelled'):
+        super(BaseSpider, self).__init__()
+        self.reason = reason
