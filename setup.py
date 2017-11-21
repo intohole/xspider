1523d7f54cc89bec9e3e40c4c81558badaea5cf7
intohole <intoblack86@gmail.com>
2017-11-21 11:11:23 +0800

    version update 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 kw = dict(
     name='xspider',
-    version='0.0.5',
+    version='0.0.6',
     description='simple spider',
     author='intohole',
     author_email='intoblack86@gmail.com',
