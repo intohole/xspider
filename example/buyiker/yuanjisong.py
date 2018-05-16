b6a66fa6c2346bf9e1ed6adbec8f91e8f9ae5b6b
intohole <intoblack86@gmail.com>
2018-05-16 21:43:46 +0800

    fix

---
+++ example/buyiker/yuanjisong.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+
+from xspider.spider.spider import BaseSpider
+from xspider.filters import UrlFilter
+from xspider.processor import PageProcessor
+from xspider.selector import XPathSelector
+from xspider.filters.CrawledFilter import SimpleCrawledFilter
+from xspider import model
+from b2 import system2
+system2.reload_utf8()
+
+
+class BuYiKr(PageProcessor.PageProcessor):
+    def __init__(self):
+        super(BuYiKr, self).__init__()
+        self.title_extractor = XPathSelector.XpathSelector(
+            path="//title/text()")
+
+    def process(self, page, spider):
+        items = model.fileds.Fileds()
+        items["title"] = self.title_extractor.find(page)
+        items["url"] = page.url
+        return items
+
+
+if __name__ == "__main__":
+    spider = BaseSpider(
+        name="buyikr",
+        crawled_filter=SimpleCrawledFilter(),
+        page_processor=BuYiKr(),
+        allow_site=["yuanjisong.com"],
+        start_urls=["https://www.yuanjisong.com/job"])
+    spider.start()
